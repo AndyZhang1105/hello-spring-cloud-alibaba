318a30f3dbbd39d2fd45130c4fb84306a6002620
Andy <andy@chengwkj.com>
2020-08-23 18:37:54 +0800

    feign with Sentinel

--- hello-spring-cloud-alibaba-nacos-consumer-feign/src/main/java/com/zz/hello/consumer/feign/service/FeignService.java
+++ hello-spring-cloud-alibaba-nacos-consumer-feign/src/main/java/com/zz/hello/consumer/feign/service/FeignService.java
@@ -1,10 +1,11 @@
 package com.zz.hello.consumer.feign.service;
 
+import com.zz.hello.consumer.feign.service.fallback.FallbackServiceImpl;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "nacos-provider")
+@FeignClient(value = "nacos-provider", fallback = FallbackServiceImpl.class)
 public interface FeignService {
     @GetMapping(value = "/test/{message}")
     String test(@PathVariable("message") String message);
---
+++ hello-spring-cloud-alibaba-nacos-consumer-feign/src/main/java/com/zz/hello/consumer/feign/service/fallback/FallbackServiceImpl.java
@@ -0,0 +1,13 @@
+package com.zz.hello.consumer.feign.service.fallback;
+
+import com.zz.hello.consumer.feign.service.FeignService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FallbackServiceImpl implements FeignService {
+
+    @Override
+    public String test(String message) {
+        return "test fallback";
+    }
+}
