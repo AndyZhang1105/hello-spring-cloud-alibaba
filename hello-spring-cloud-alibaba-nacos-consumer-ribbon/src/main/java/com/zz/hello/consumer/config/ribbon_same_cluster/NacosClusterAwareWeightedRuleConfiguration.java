15df83ec297971fb80932d3d338979ad2f54525b
Andy <andy@chengwkj.com>
2020-09-06 21:17:32 +0800

    ribbon, but need more debug for same cluster and meta configurations

--- hello-spring-cloud-alibaba-nacos-consumer-ribbon/src/main/java/com/zz/hello/consumer/config/ribbon_same_cluster/NacosClusterAwareWeightedRuleConfiguration.java
+++ hello-spring-cloud-alibaba-nacos-consumer-ribbon/src/main/java/com/zz/hello/consumer/config/ribbon_same_cluster/NacosClusterAwareWeightedRuleConfiguration.java
@@ -3,7 +3,7 @@ package com.zz.hello.consumer.config.ribbon_same_cluster;
 import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
-//@RibbonClients(defaultConfiguration = ClusterRibbonConfiguration.class)
+@Configuration
+@RibbonClients(defaultConfiguration = ClusterRibbonConfiguration.class)
 public class NacosClusterAwareWeightedRuleConfiguration {
 }
--- hello-spring-cloud-alibaba-nacos-consumer-ribbon/src/main/java/com/zz/hello/consumer/config/ribbon_weight/RibbonConfiguration.java
+++ hello-spring-cloud-alibaba-nacos-consumer-ribbon/src/main/java/com/zz/hello/consumer/config/ribbon_weight/RibbonConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RibbonConfiguration {
 
-    @Bean
+//    @Bean
     public IRule ribbonRule() {
         return new NacosWeightedRule();
     }
